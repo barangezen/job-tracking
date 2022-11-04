cecbc6336f7dc437612a3f7b0a0f08f9d846b343
barangezen <kartalbarans3@hotmail.com>
2022-11-04 22:27:29 +0300

    Create new job section layout complated without logic

--- src/page/MainContent.tsx
+++ src/page/MainContent.tsx
@@ -1,18 +1,36 @@
-import { Container } from "@mui/material";
-import { CustomTextField } from "../components/Header/TextField/TextField";
 import styles from "./MainContent.module.scss";
+import { Button, Container } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import { Dropdown } from "../components/Dropdown/Dropdown";
+import { CustomTextField } from "../components/TextField/TextField";
+import { Stack } from "@mui/system";
+import { List } from "../components/List/List";
 
 export const MainContent: React.FC = () => {
   return (
     <Container className={styles.container}>
       <span className={styles.title}>Create New Job</span>
-      <div>
-        <CustomTextField
-          title={"Job Name"}
-          size="small"
-          className={styles.jobNameCreate}
-        />
-      </div>
+      <Stack className={styles.createJobContainer}>
+        <div className={styles.jobNameCreate}>
+          <CustomTextField title={"Job Name"} size="small" />
+        </div>
+        <div className={styles.priortySelect}>
+          <Dropdown />
+        </div>
+        <Button
+          className={styles.createButton}
+          size="medium"
+          variant="contained"
+          color="primary"
+          startIcon={<AddIcon />}
+        >
+          Create
+        </Button>
+      </Stack>
+      <Stack>
+        <span className={styles.jobListTitle}>Job List</span>
+        <List />
+      </Stack>
     </Container>
   );
 };
